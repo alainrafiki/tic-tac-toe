a5f16096fbdfb9caade7b625fa7f7c5cfd5e86ae
alainrafiki <6798298+mydeveloper1@users.noreply.github.com>
2020-01-06 23:09:44 -0600

    Save current progress

--- src/index.js
+++ src/index.js
@@ -21,12 +21,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
     return <Square value={i} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: :P';
 
     return (
       <div>
